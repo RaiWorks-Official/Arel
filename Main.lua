c65aa349017c88ac7c5571e2c20af2fa81b5a475
RaiWorks-Official <ralphwinworks@gmail.com>
2026-08-19 20:32:05 +0800

    update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,18 @@
 -- loader
-local Players = game:GetService("Players")
+-- local Players = game:GetService("Players")
+local Id = game.PlaceId
+
+local Supported = {
+  ["Legends of Speed"] = {
+    Id = ,
+    File = "LOS.luau",
+  },
+}
+
+for i, v in ipairs(Supported) do
+  if Id == v.Id then
+    loadstring()()
+  else
+    error("Not Supported")
+  end
+end
